e42d42cf13df353e2eccc92fc2937efad161372c
AntoineLemlin <ant.lemlin@gmail.com>
2021-08-12 17:25:45 +0200

    edit and delete

--- JS/create.js
+++ JS/create.js
@@ -1,5 +1,9 @@
+// Array of all inputs which stocks all the values in the order of the HTML
 const inputs = Array.from(document.querySelectorAll(".get-data"));
 let base64String = "";
+// Takes the data after the ? in the url (here the id)
+let queryString = location.search.substring(1);
+let exist = false;
 
 function imageUploaded() {
   var file = document.querySelector("input[type=file]")["files"][0];
@@ -16,6 +20,24 @@ function imageUploaded() {
   reader.readAsDataURL(file);
 }
 
+document.addEventListener("DOMContentLoaded", async function () {
+  await getURL();
+
+  obj.forEach(({ id, image, name, shortDescription, description }) => {
+    if (id === queryString) {
+      exist = true;
+
+      base64String = image;
+      console.log(image);
+      document.getElementById(
+        "preview"
+      ).src = `data:image/jpeg;base64,${image}`;
+      document.getElementById("fname").value = name;
+      document.getElementById("short-description").value = shortDescription;
+      document.getElementById("description").value = description;
+    }
+  });
+});
 
 // upload
 document
@@ -28,23 +50,42 @@ document
       return;
     }
 
-    const [name, shortDescription, description] = values; 
+    const [name, shortDescription, description] = values;
 
-    const response = await fetch(
-      "https://character-database.becode.xyz/characters",
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          image: base64String,
-          name,
-          shortDescription,
-          description,
-        }),
-      }
-    );
+    if (!exist) {
+      const response = await fetch(
+        "https://character-database.becode.xyz/characters",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            image: base64String,
+            name,
+            shortDescription,
+            description,
+          }),
+        }
+      );
+    } else {
+      const response = await fetch(
+        `https://character-database.becode.xyz/characters/${queryString}`,
+        {
+          method: "PUT",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            id: queryString,
+            name,
+            shortDescription,
+            description,
+            image: base64String,
+          }),
+        }
+      ).then(success => alert('Success')).catch(console.error("There was an error"));
+    }
 
-    document.location.href="index.html"
+        document.location.href = "index.html"
   });
--- JS/view.js
+++ JS/view.js
@@ -1,8 +1,7 @@
+let queryString = location.search.substring(1);
+
 document.addEventListener("DOMContentLoaded", async function () {
   // I get the id from the url and remove the ?
-  let queryString = location.search.substring(1);
-
-  console.log(queryString);
 
   await getURL();
 
@@ -14,6 +13,29 @@ document.addEventListener("DOMContentLoaded", async function () {
       document.getElementById("name").innerHTML = name;
       document.getElementById("short-description").innerHTML = shortDescription;
       document.getElementById("description").innerHTML = description;
+      document.getElementById(
+        "btn-update"
+      ).href = `create_character.html?${id}`;
     }
   });
 });
+
+document
+  .getElementById("btn-delete")
+  .addEventListener("click", async function () {
+    if (confirm("Do you really want to delete this?")) {
+      const response = await fetch(
+        `https://character-database.becode.xyz/characters/${queryString}`,
+        {
+          method: "DELETE",
+          headers: {
+            "Content-type": "application/json", // Indicates the content
+          },
+        }
+      )
+        .then((res) => console.log("Successfuly deleted"))
+        .catch((error) => console.error(error)); // or res.json()
+
+      document.location.href = "index.html";
+    }
+  });
